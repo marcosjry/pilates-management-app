4b10f1473de06baa07dede372c39b5f40ba6523f
Marcos Antônio <marcosjunior.me.009@gmail.com>
2025-05-16 10:07:21 -0300

    feat: adicionado DTO e Exceção para Frequência/Aula

---
+++ src/main/java/com/user/managament/DTO/classroom/FrequencyClassToCreateDTO.java
@@ -0,0 +1,21 @@
+package com.user.managament.DTO.classroom;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public record FrequencyClassToCreateDTO(
+
+        @NotNull(message = "A Aula não pode ser NULL.")
+        UUID classroomId,
+
+        @NotNull(message = "O Aluno não pode ser NULL.")
+        UUID customerId,
+
+        @NotNull(message = "A data não pode ser NULL.")
+        LocalDate classDate
+
+        ) {
+}
