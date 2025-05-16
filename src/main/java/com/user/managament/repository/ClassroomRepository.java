2b57929c6c7665cbdaa4d1afb883a5fb5e43cc77
Marcos Antônio <marcosjunior.me.009@gmail.com>
2025-05-16 10:05:28 -0300

    feat: adicionado service e repository para Turma

---
+++ src/main/java/com/user/managament/repository/ClassroomRepository.java
@@ -0,0 +1,29 @@
+package com.user.managament.repository;
+
+import com.user.managament.DTO.classroom.ClassroomWithCountDTO;
+import com.user.managament.model.classroom.Classroom;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+public interface ClassroomRepository extends JpaRepository<Classroom, UUID> {
+
+    @Query("""
+    SELECT new com.user.managament.DTO.classroom.ClassroomWithCountDTO(
+        c.id,
+        c.startTime,
+        c.classroomType,
+        COUNT(cf.customer)
+    )
+    FROM ClassroomFrequency cf
+    JOIN cf.classroom c
+    WHERE cf.classroomDate = :date
+    GROUP BY c.id, c.startTime, c.classroomType
+    """)
+    List<ClassroomWithCountDTO> findClassroomsWithStudentCountByDate(@Param("date") LocalDate date);
+
+}
---
+++ src/main/java/com/user/managament/services/ClassroomService.java
@@ -0,0 +1,24 @@
+package com.user.managament.services;
+
+import com.user.managament.DTO.classroom.ClassroomToCreateDTO;
+import com.user.managament.DTO.classroom.ClassroomWithCountDTO;
+import com.user.managament.DTO.classroom.ReqClassroomWithCountDTO;
+import com.user.managament.exception.ClassroomDoesntExistsException;
+import com.user.managament.model.classroom.Classroom;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+public interface ClassroomService {
+
+    void doesCreateClassroom(ClassroomToCreateDTO classroomToCreateDTO);
+    void doesDeleteClassroom(UUID classroomId);
+
+    Classroom findClassroomById(UUID classroomId) throws ClassroomDoesntExistsException;
+
+    List<ClassroomWithCountDTO> findClassroomsWithStudents(LocalDate date);
+    void deleteClassroomById(UUID classroomId);
+
+
+}
