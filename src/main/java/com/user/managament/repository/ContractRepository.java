f33de63039b48aa3ec72e0e680ca5efe4ef29521
Marcos Antônio <marcosjunior.me.009@gmail.com>
2025-05-16 11:26:51 -0300

    feat: adicionando novos métodos para consulta no banco

--- src/main/java/com/user/managament/repository/ContractRepository.java
+++ src/main/java/com/user/managament/repository/ContractRepository.java
@@ -1,10 +1,13 @@
 package com.user.managament.repository;
 
+import com.user.managament.DTO.classroom.ClassroomWithCountDTO;
+import com.user.managament.DTO.contract.ActiveContractsWithCustomersDTO;
 import com.user.managament.model.contract.Contract;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -24,5 +27,12 @@ public interface ContractRepository extends JpaRepository<Contract, UUID> {
     List<Contract> getActiveContractsFromCustomerId(@Param("customerId") UUID customerId);
 
     Optional<Contract> findTopByCustomerIdOrderByInitDateDesc(UUID customerId);
+
+    @Query(value = """
+    SELECT COUNT(tc.id)
+    FROM table_contract tc
+    JOIN table_customer cu ON cu.id = tc.customer_id
+    """, nativeQuery = true)
+    Long getDashboardTotalsActiveContracts();
 
 }
