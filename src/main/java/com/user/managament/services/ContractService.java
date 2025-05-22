c912bc4efe2767154d2a9c7366ad161d983ef6bb
Marcos Antônio <marcosjunior.me.009@gmail.com>
2025-05-22 13:26:24 -0300

    feat: implementado novos métodos para o contrato

--- src/main/java/com/user/managament/services/ContractService.java
+++ src/main/java/com/user/managament/services/ContractService.java
@@ -1,9 +1,6 @@
 package com.user.managament.services;
 
-import com.user.managament.DTO.contract.ActiveContractsWithCustomersDTO;
-import com.user.managament.DTO.contract.ContractDTO;
-import com.user.managament.DTO.contract.ContractToCreateDTO;
-import com.user.managament.DTO.contract.ContractToEditDTO;
+import com.user.managament.DTO.contract.*;
 import com.user.managament.exception.ContractDoesntExistsException;
 import com.user.managament.model.contract.Contract;
 
@@ -11,9 +8,12 @@ import java.util.List;
 import java.util.UUID;
 
 public interface ContractService {
+
     void doesCreateContract(ContractToCreateDTO contractToCreateDTO);
     void doesEditContract(UUID id, ContractToEditDTO contractToEditDTO);
     void doesDeleteContract(UUID contractId);
+
+    List<ContractsAndCustomerDTO> getContracts(String query);
 
     List<ContractDTO> getContractsFromCustomerId(UUID customerId);
     List<ContractDTO> findLastContractByCustomerId(UUID customerId);
@@ -24,4 +24,6 @@ public interface ContractService {
 
     void deleteById(UUID id);
     Contract findById(UUID id) throws ContractDoesntExistsException;
+
+    List<ContractsExpiring> findExpiringContracts();
 }
